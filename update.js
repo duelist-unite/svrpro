25535756a304c43381b1fbac8d8a0c81da4521f1
William Sedlacek <wsedlacekc@gmail.com>
2019-08-09 20:15:15 +0000

    Auto Update - Fix

--- update.js
+++ update.js
@@ -2,9 +2,9 @@ var moment = require('moment');
 var exec = require('child_process').exec;
 var check = function () {
   var now = moment();
-  if (now.hour() == 4 && now.minute() == 35) {
-    console.log("Time to restart!");
-    exec('./restart.sh');
+  if (now.hour() == 3 && now.minute() == 35) {
+    console.log("Time to update!");
+    exec('./update.sh');
     setTimeout(check, 59000);
   } else {
     setTimeout(check, 59000);
