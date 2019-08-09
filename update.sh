a93aa93370771831b668b57700a03e2a8e3dc11b
William Sedlacek <wsedlacekc@gmail.com>
2019-08-09 22:00:11 +0000

    Update srvpro as well

--- update.sh
+++ update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+git add -A
+git reset --hard
+git pull
+
 git submodule foreach "git add -A && git reset --hard"
 git fetch origin && git add -A && git reset --hard && git checkout origin/master
+
 git submodule sync --recursive
 git submodule update --recursive --init
 git submodule update --recursive --remote
+
 pm2 start all
